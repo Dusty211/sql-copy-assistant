6ae85fcd89a811c050ff3a4b60a3945b41e871dd
Kyle Houghton <kyle@keentocode.dev>
2023-10-02 23:09:32 -0500

    Defining callback in separate file.

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const {iterateFileLines} = require('./utils/iterate-file-lines')
+const {iterateFileLinesCb} = require('./utils/iterate-file-lines-cb')
 
 async function main(){
     console.time('main() exec time')
-    await iterateFileLines(4, line => {
-        const parsed = JSON.parse(line)
-        console.log('===========>> NEW THREAD:')
-        console.log(parsed.posts)
-        return
-    })
+    await iterateFileLines(4, iterateFileLinesCb)
     console.timeEnd('main() exec time')
 }
 
