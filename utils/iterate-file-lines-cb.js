f51819394d2f09cc4cf175c0bf318e3c5bccb91a
Kyle Houghton <kyle@keentocode.dev>
2023-10-04 00:50:53 -0500

    testing iterateFileLinesCb() success.

--- utils/iterate-file-lines-cb.js
+++ utils/iterate-file-lines-cb.js
@@ -1,11 +1,13 @@
 const {convertHtml} = require('./html-to-text')
+const {toSqlSafeText} = require('./to-sql-safe-text')
 
 async function iterateFileLinesCb(line, batchCount) {
     console.log(batchCount.value)
     const parsed = JSON.parse(line)
     console.log('===========>> NEW THREAD:')
     console.log(parsed.posts.map(post => {
-        post.text = convertHtml(post.com)
+        const text = convertHtml(post.com)
+        post.com = toSqlSafeText(text)
         return post
     }))
     ++batchCount.value
