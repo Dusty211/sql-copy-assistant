14e7bf73c9f311bb77ef045a2785e5be80e05fc5
Kyle Houghton <kyle@keentocode.dev>
2023-10-04 00:38:33 -0500

    toSqlSafeText() and tests implemented and working.

--- utils/__tests__/to-sql-safe-text.test.js
+++ utils/__tests__/to-sql-safe-text.test.js
@@ -4,40 +4,48 @@ describe(`toSqlSafeText() ==>`, () => {
 
     test(`Result of toSqlSafeText() is defined`, () => {
         expect(toSqlSafeText('test')).toBeDefined();
-    });
+    })
 
     //Literal '\' character is escaped    
     test(`'\\' is converted to '\\\\'`, () => {
         expect(toSqlSafeText('\\')).toBe('\\\\');
-    });
+    })
     
     //Newline converted
     test(`'\\n' is converted to ' '`, () => {
         expect(toSqlSafeText('\n')).toBe(' ');
-    });
+    })
 
     //Carriage return converted
     test(`'\\r' is converted to ' '`, () => {
         expect(toSqlSafeText('\r')).toBe(' ');
-    });
+    })
 
     //Tab converted
     test(`'\\t' is converted to ' '`, () => {
         expect(toSqlSafeText('\t')).toBe(' ');
-    });
+    })
 
     //Backspace converted
     test(`'\\b' is converted to ''`, () => {
         expect(toSqlSafeText('\b')).toBe('');
-    });
+    })
 
     //Form feed converted
     test(`'\\f' is converted to ' '`, () => {
         expect(toSqlSafeText('\f')).toBe(' ');
-    });
+    })
 
     //Vertical tab converted
     test(`'\\v' is converted to ' '`, () => {
         expect(toSqlSafeText('\v')).toBe(' ');
-    });    
-});
+    })
+
+    //Multiple instances of all special characters randomly interdispersed
+    test(`Multiple items are handled properly`, () => {
+        const maniacalString = 'begin\\sa\rsas\tuisd\\\rfafm\vnt\btty\nu\\\nit\vydf\fdfaf\\\ras\bd\\\nfsngad\nfa\rfa\fsd\tf\\end'
+        expect(toSqlSafeText(maniacalString)).toBe(
+            'begin\\\\sa sas uisd fafm nttty u it ydf dfaf asd fsngad fa fa sd f\\\\end'
+        );
+    })
+})
--- utils/to-sql-safe-text.js
+++ utils/to-sql-safe-text.js
@@ -1,5 +1,46 @@
-function toSqlSafeText(string){
-    return string
+//The solutions to break this regex into mupliple lines were not worth the tradeoffs.
+const SqlSafeTextRegex = 
+    /(?<dataNewline>\\\n)|(?<dataCarriageReturn>\\\r)|(?<literalBackslash>\\)|(?<newline>\n)|(?<tab>\t)|(?<carriageReturn>\r)|(?<backspace>[\b])|(?<formFeed>\f)|(?<verticalTab>\v)/g
+
+function SqlSafeTextCb(match, ...args){        
+    const groups = args.at(-1) //groups existing at -1 depends on regex containing named groups.
+
+    const needsEscapedBackslash = (
+        groups.literalBackslash
+    ) ? true : false
+
+    const needsRemoval = (
+        groups.backspace
+    ) ? true : false
+
+    const needsSpace = (
+        groups.dataNewline || 
+        groups.dataCarriageReturn || 
+        groups.newline || 
+        groups.tab || 
+        groups.carriageReturn || 
+        groups.formFeed || 
+        groups.verticalTab
+    ) ? true : false
+
+    if(needsEscapedBackslash){
+        return '\\\\'
+    }
+    
+    if(needsRemoval){
+        return ''
+    }
+
+    if(needsSpace){
+        return ' '
+    }
+
+    //Should never reach here unless someone adds to the regex and does not update the named group conditionals:
+    throw new Error(`Unexpected: toSqlSafeText() had an unexpected named group match within replaceCb().`)
+}
+
+function toSqlSafeText(string){    
+    return string.replace(SqlSafeTextRegex, SqlSafeTextCb)
 }
 
 module.exports = {toSqlSafeText}
