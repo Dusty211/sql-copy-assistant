90f63287f36c99e4e270f8aa8a860de0692abc19
Kyle Houghton <kyle@keentocode.dev>
2023-10-03 20:27:17 -0500

    Basic test initial setup.

--- utils/__tests__/to-sql-safe-text.test.js
+++ utils/__tests__/to-sql-safe-text.test.js
@@ -1,8 +1,36 @@
 const {toSqlSafeText} = require('../to-sql-safe-text')
 
-describe('toSqlSafeText() ==>', () => {
+describe(`toSqlSafeText() ==>`, () => {
 
-    test('Result of toSqlSafeText() is defined', () => {
+    test(`Result of toSqlSafeText() is defined`, () => {
         expect(toSqlSafeText('test')).toBeDefined();
+    });
+    
+    test(`'\\' is converted to '\\\\'`, () => {
+        expect(toSqlSafeText('\\')).toBe('\\');
+    });
+    
+    test(`'\\n' is converted to ' '`, () => {
+        expect(toSqlSafeText('\n')).toBe('\n');
+    });
+
+    test(`'\\r' is converted to ' '`, () => {
+        expect(toSqlSafeText('\r')).toBe('\r');
+    });
+
+    test(`'\\t' is converted to ' '`, () => {
+        expect(toSqlSafeText('\t')).toBe('\t');
+    });
+
+    test(`'\\b' is converted to ''`, () => {
+        expect(toSqlSafeText('\b')).toBe('\b');
+    });
+
+    test(`'\\f' is converted to ''`, () => {
+        expect(toSqlSafeText('\f')).toBe('\f');
+    });
+
+    test(`'\\v' is converted to ' '`, () => {
+        expect(toSqlSafeText('\v')).toBe('\v');
     });    
 });
