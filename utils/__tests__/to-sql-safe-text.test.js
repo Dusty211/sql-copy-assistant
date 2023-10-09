fdb1b288f1fb28bd91c0af1563ae678e0e1c2a87
Kyle Houghton <kyle@keentocode.dev>
2023-10-09 12:07:04 -0500

    Updated tests with 'Empty string works.'

--- utils/__tests__/to-sql-safe-text.test.js
+++ utils/__tests__/to-sql-safe-text.test.js
@@ -3,42 +3,42 @@ const {toSqlSafeText} = require('../to-sql-safe-text')
 describe(`toSqlSafeText() ==>`, () => {
 
     test(`Result of toSqlSafeText() is defined`, () => {
-        expect(toSqlSafeText('test')).toBeDefined();
+        expect(toSqlSafeText('test')).toBeDefined()
     })
 
     //Literal '\' character is escaped    
     test(`'\\' is converted to '\\\\'`, () => {
-        expect(toSqlSafeText('\\')).toBe('\\\\');
+        expect(toSqlSafeText('\\')).toBe('\\\\')
     })
     
     //Newline converted
     test(`'\\n' is converted to ' '`, () => {
-        expect(toSqlSafeText('\n')).toBe(' ');
+        expect(toSqlSafeText('\n')).toBe(' ')
     })
 
     //Carriage return converted
     test(`'\\r' is converted to ' '`, () => {
-        expect(toSqlSafeText('\r')).toBe(' ');
+        expect(toSqlSafeText('\r')).toBe(' ')
     })
 
     //Tab converted
     test(`'\\t' is converted to ' '`, () => {
-        expect(toSqlSafeText('\t')).toBe(' ');
+        expect(toSqlSafeText('\t')).toBe(' ')
     })
 
     //Backspace converted
     test(`'\\b' is converted to ''`, () => {
-        expect(toSqlSafeText('\b')).toBe('');
+        expect(toSqlSafeText('\b')).toBe('')
     })
 
     //Form feed converted
     test(`'\\f' is converted to ' '`, () => {
-        expect(toSqlSafeText('\f')).toBe(' ');
+        expect(toSqlSafeText('\f')).toBe(' ')
     })
 
     //Vertical tab converted
     test(`'\\v' is converted to ' '`, () => {
-        expect(toSqlSafeText('\v')).toBe(' ');
+        expect(toSqlSafeText('\v')).toBe(' ')
     })
 
     //Multiple instances of all special characters randomly interdispersed
@@ -46,6 +46,11 @@ describe(`toSqlSafeText() ==>`, () => {
         const maniacalString = 'begin\\sa\rsas\tuisd\\\rfafm\vnt\btty\nu\\\nit\vydf\fdfaf\\\ras\bd\\\nfsngad\nfa\rfa\fsd\tf\\end'
         expect(toSqlSafeText(maniacalString)).toBe(
             'begin\\\\sa sas uisd fafm nttty u it ydf dfaf asd fsngad fa fa sd f\\\\end'
-        );
+        )
+    })
+
+    //Empty string works:
+    test(`Empty string works`, () => {
+        expect(toSqlSafeText('')).toBe('')
     })
 })
