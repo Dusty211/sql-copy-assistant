d85dae8406ad169a3b6fd3239fb9c77637d6f7e6
Kyle Houghton <kyle@keentocode.dev>
2023-10-09 12:02:42 -0500

    Index.js removed comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {iterateFileLinesCb} = require('./utils/iterate-file-lines-cb')
 
 async function main(){
     console.time('main() exec time')
-    await iterateFileLines(3397, iterateFileLinesCb)
+    await iterateFileLines(iterateFileLinesCb, null, 5000)
     console.timeEnd('main() exec time')
     return
 }
