78c68d61a67fc48e5db4053265b3cecfd886dd21
Kyle Houghton <kyle@keentocode.dev>
2023-10-10 01:01:26 -0500

    Stager latest changes.

--- utils/Stager.js
+++ utils/Stager.js
@@ -1,12 +1,25 @@
-function Stager(){
+const { appendFile } = require('node:fs/promises')
+const path = require('path')
+
+function Stager(outputDir){
+    this.outputDir = outputDir
+    this.fileTimestamp = Date.now()
     this.tableData = {}
     this.dataMapFunctions = {}
 
     this.createTable = function(name, dataMapFunction){
-            this.tableData[name] = ''
-            this.dataMapFunctions[name] = (rowInput, index) => {
-                this.tableData[name] += dataMapFunction(rowInput, index)
-            }
+        this.tableData[name] = ''
+        this.dataMapFunctions[name] = async (rowInput, index) => {
+            this.tableData[name] += await dataMapFunction(rowInput, index)
+        }
+        return this
+    }
+
+    this.writeDataFiles = async function(){
+        const createFilePath = fileName => path.join(this.outputDir, `${this.fileTimestamp}-${fileName}`)
+        const appendDataPromises = Object.keys(this.tableData).map(name => appendFile(createFilePath(name), this.tableData[name]))
+        await Promise.all(appendDataPromises)
+        this.clearData()
         return this
     }
 
