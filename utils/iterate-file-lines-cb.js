aa532952cb1e77921f66da7245739395cb66a9e7
Kyle Houghton <kyle@keentocode.dev>
2023-10-09 22:40:36 -0500

    iterate-file-lines-cb.js before refactor.

--- utils/iterate-file-lines-cb.js
+++ utils/iterate-file-lines-cb.js
@@ -1,16 +1,18 @@
-const {convertHtml} = require('./html-to-text')
-const {toSqlSafeText} = require('./to-sql-safe-text')
+const {stager} = require('./stage-tables')
 
-async function iterateFileLinesCb(line, batchCount) {
-    console.log(batchCount.value)
+async function iterateFileLinesCb(line, batch, batchMax) {
     const parsed = JSON.parse(line)
-    console.log('===========>> NEW THREAD:')
-    console.log(parsed.posts.map(post => {
-        const text = convertHtml(post.com)
-        post.com = toSqlSafeText(text)
-        return post
-    }))
-    ++batchCount.value
+
+    const dbIndex = batch.currentIndex + 1
+    stager.dataMapFunctions.thread(parsed, dbIndex)
+    stager.dataMapFunctions.post(parsed, dbIndex)
+
+    batch.increment()
+    console.log(`batch.count: ${batch.count}`)
+    if(batch.count >= batchMax){
+        await stager.writeDataFiles()
+        batch.reset()
+    }
     return
 }
 
