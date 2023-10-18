373ed699ab63a8464bec22dedd628cc70e249441
Kyle Houghton <kyle@keentocode.dev>
2023-10-18 15:06:30 -0500

    Multithreading refactor to iterate-file-lines.js and index.js.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const {iterateFileLines} = require('./utils/iterate-file-lines')
-const {iterateFileLinesCb} = require('./utils/iterate-file-lines-cb')
 
 async function main(){
     console.time('main() exec time')
-    await iterateFileLines(iterateFileLinesCb, null, 5000)
+    await iterateFileLines(null, 5000)
     console.timeEnd('main() exec time')
     return
 }
--- utils/iterate-file-lines.js
+++ utils/iterate-file-lines.js
@@ -3,20 +3,29 @@ const path = require('path')
 const readline = require('readline')
 
 const {Batch} = require('./Batch')
+const {sendBatchToWorkerThreads} = require('./send-batch-to-worker-threads')
+const mapDataFunctions = require('./stage-table-functions')
 
 const {sourceDir} = require('../paths.json')
 const DATAFILE = path.join(__dirname, sourceDir)
+const tableNames = Object.keys(mapDataFunctions)
 
-async function iterateFileLines(cb, max = null, batchMax = 5000){
-
+async function iterateFileLines(max = null, batchMax = 5000){
     try{
+        const handleData = workerThreadResults => new Promise((resolve) => {
+            setTimeout(() => resolve('test timeout promise'), 800)
+        })
+
         const rl = readline.createInterface({
             input: fs.createReadStream(DATAFILE),
             crlfDelay: Infinity
         })
 
         const batch = new Batch()
-        let appendDataToFileFn
+        const nextData = []
+        let currentData = []
+        let workerThreadPromises = []
+        let workerThreadResults = []
 
         for await (const line of rl) {
             const dbIndex = batch.currentIndex + 1
@@ -24,16 +33,39 @@ async function iterateFileLines(cb, max = null, batchMax = 5000){
             if(typeof max === 'number' && batch.currentIndex >= max){
                 break
             }else{
-                appendDataToFileFn = await cb(line, dbIndex)
+                nextData.push([dbIndex, line])
                 batch.increment()
                 if(batch.count >= batchMax){
-                    await appendDataToFileFn()
+                    workerThreadResults = await workerThreadPromises
+                    handleData(workerThreadResults) //will be an empty string on first batch.
+                    workerThreadPromises = Promise.all(tableNames.map(tableName => {
+                        return sendBatchToWorkerThreads(tableName, currentData)
+                    }))
+                    currentData = [...nextData]
+                    nextData.length = 0
                     batch.reset()
                 }
                 batch.incrementCurrentIndex()
             }
         }
-        await appendDataToFileFn()
+        //Handle data still coming back from last loop of for loop:
+        workerThreadResults = await workerThreadPromises
+        handleData(workerThreadResults)
+
+        //Remaining unprocessed 'currentData':
+        workerThreadPromises = Promise.all(tableNames.map(tableName => {
+            return sendBatchToWorkerThreads(tableName, currentData)
+        }))
+        workerThreadResults = await workerThreadPromises
+        handleData(workerThreadResults)
+
+        //Remaining unprocessed 'nextData':
+        workerThreadPromises = Promise.all(tableNames.map(tableName => {
+            return sendBatchToWorkerThreads(tableName, nextData) //Promise.all
+        }))
+        workerThreadResults = await workerThreadPromises
+        await handleData(workerThreadResults)
+
         batch.reset()
     }catch(e){
         console.error(e)
